471c5b0d24bf7f2a11ba46a85bbcb02a505c1ee7
Denny Christochowitz <denny.christochowitz@gmail.com>
2017-07-13 21:52:56 +0200

    reactive form validation

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
-  templateUrl: 'app/user/profile.component.html'
+  templateUrl: 'app/user/profile.component.html',
+  styles: [`
+    em { float: right; color: #e05c65; padding-left: 10px; }
+    .error input { background-color: #e3c3e5; }
+    .error ::-webkit-input-placeholder { color: #999; }
+  `]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
 
   constructor (private auth: AuthService, private router: Router) {
     const user = this.auth.currentUser;
   }
 
   ngOnInit() {
-    const firstName = new FormControl(this.auth.currentUser.firstName);
-    const lastName = new FormControl(this.auth.currentUser.lastName);
+    this.firstName = new FormControl(this.auth.currentUser.firstName, Validators.required);
+    this.lastName = new FormControl(this.auth.currentUser.lastName, Validators.required);
     this.profileForm = new FormGroup({
-      firstName,
-      lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     });
   }
 
@@ -27,7 +34,17 @@ export class ProfileComponent implements OnInit {
   }
 
   saveProfile(formValues) {
-    this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['/events']);
+    if (this.profileForm.valid) {
+      this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['/events']);
+    }
+  }
+
+  isInvalidLastName() {
+    return this.lastName.invalid && this.lastName.touched;
+  }
+
+  isInvalidFirstName() {
+    return this.firstName.invalid && this.firstName.touched;
   }
 }
